ebd42fb066c16c869f27a2c1a1971f5e1555b918
iRupam <27852445+iRupam@users.noreply.github.com>
2021-11-10 21:00:19 +0530

    No choice but to commit.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React,{useState} from 'react'
 import TodoForm from './TodoForm'
+import Todo from './Todo';
 function TodoList() {
     const {todos,setTodos}=useState([])
     
@@ -12,13 +13,22 @@ function TodoList() {
         setTodos(newTodos);
     
     };
-
+    const completeTodo=id=>{
+        let updatedTodos=todos.map(todo=>{
+            if(todo.id===id){
+                todo.isComplete=!todo.isComplete;
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
+    }
     return (
         <div>
             <h1>What's the Plan for Today?</h1>
             <TodoForm onSubmit={addTodo}/>
+            <Todo todos={todos} completeTodo={completeTodo}/> 
         </div>
-    )
+    );
 }
 
 export default TodoList
